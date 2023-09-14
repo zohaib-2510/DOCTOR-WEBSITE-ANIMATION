fe3d61739ecd04400ea215f44ae75dac08708ec9
zohaib-2510 <140811807+zohaib-2510@users.noreply.github.com>
2023-09-14 09:23:49 -0700

    about page completed

---
+++ about.js
@@ -0,0 +1,4 @@
+function sbKa(){
+    var a = document .getElementById('orp').innerHTML='FACERE TOTAM! NULLA NOBIS ILLO RERUM SED EX MAIORES FACILIS MINUS AT? EXERCITATIONEM MAGNI QUOD NEQUE NUMQUAM REICIENDIS ATQUE POSSIMUS TEMPORA IMPEDIT, QUAM NEMO A'
+    document.getElementById('nbnb').style.display='none'
+}
